a6ffdd5dbbddd3aee6a1f03262f70e7bd70098ea
David Porter <david.porter@anu.edu.au>
2010-07-29 14:44:12 +1000

    Use the anu-ldap gem

--- user-details.rb
+++ user-details.rb
@@ -3,27 +3,21 @@
 require "rubygems"
 require "sinatra"
 require "haml"
-require "net/ldap"
+require "anu-ldap"
 
 get "/" do
   haml :search
 end
 
 get "/search" do
-  if params[:email] or params[:uni_id]
-    filter = Net::LDAP::Filter.eq("mail", params[:email])  if params[:email]
-    filter = Net::LDAP::Filter.eq("uid", params[:uni_id])  if params[:uni_id]
-
-    ldap = Net::LDAP.new(:host       => "ldap.anu.edu.au",
-                         :port       => 636,
-                         :encryption => :simple_tls)
-
-    @entries = ldap.search(:base       => "ou=people,o=anu.edu.au",
-                           :filter     => filter,
-                           :attributes => ["mail", "cn", "uid"])
-
-    @msg = "No results"  if @entries.empty?
+  @entries = []
+  if params[:email]
+    @entries = AnuLdap.find_by_email(params[:email])
+  elsif params[:uni_id]
+    @entries = AnuLdap.find_by_uni_id(params[:uni_id])
   end
+
+  @msg = "No results"  if @entries.empty?
 
   haml :search
 end
@@ -55,10 +49,10 @@ __END__
           %thead
             %th{:style => "text-align: left;"} Name
             %th{:style => "text-align: left;"} Email
-            %th{:style => "text-align: left;"} Uni ID(s)
+            %th{:style => "text-align: left;"} Uni ID
           %tbody
             - @entries.each do |e|
               %tr
-                %td{:style => "padding-right: 10px;"}= e.cn.first
-                %td{:style => "padding-right: 10px;"}= e.mail.first
-                %td= e.uid.join(", ")
+                %td{:style => "padding-right: 10px;"}= e[:full_name]
+                %td{:style => "padding-right: 10px;"}= e[:email]
+                %td= e[:uni_id]
